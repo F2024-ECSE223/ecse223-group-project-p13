82a683909080dc1d0c45e78d2493750588936cd7
Dimitri Christopoulos <154035020+DimitriC3@users.noreply.github.com>
2024-10-06 16:37:00 -0400

    started feature3

--- src/main/java/ca/mcgill/ecse/coolsupplies/controller/CoolSuppliesFeatureSet3Controller.java
+++ src/main/java/ca/mcgill/ecse/coolsupplies/controller/CoolSuppliesFeatureSet3Controller.java
@@ -2,22 +2,58 @@ package ca.mcgill.ecse.coolsupplies.controller;
 
 import java.util.List;
 
+// Model Import
+import ca.mcgill.ecse.coolsupplies.model.*;
+
+
 public class CoolSuppliesFeatureSet3Controller {
 
+  private static CoolSupplies coolSupplies;
+  private static List<Item> items;
+
   public static String addItem(String name, int price) {
-    throw new UnsupportedOperationException("Not implemented yet.");
+    
+    // Create new item
+    Item new_item = new Item(name, price, coolSupplies);
+    items.add(new_item);
+
   }
 
   public static String updateItem(String name, String newName, int newPrice) {
-    throw new UnsupportedOperationException("Not implemented yet.");
+    
+    List<Item> items = coolSupplies.getItems();
+
+    for (Item item : items) {
+      if (item.getName().equals(name)) {
+        item.setName(newName);
+        item.setPrice(newPrice);
+      }
+    }
+
   }
 
   public static String deleteItem(String name) {
-    throw new UnsupportedOperationException("Not implemented yet.");
+
+    List<Item> items = coolSupplies.getItems();
+
+    for (Item item : items) {
+      if (item.getName().equals(name)) {
+        item = null;
+      }
+    }
   }
 
   public static TOItem getItem(String name) {
-    throw new UnsupportedOperationException("Not implemented yet.");
+
+    List<Item> items = coolSupplies.getItems();
+
+    for (Item item : items) {
+      if (item.getName().equals(name)) {
+        TOItem to_item = new TOItem(item.getName(), item.getPrice());
+        return to_item;
+      }
+    }
+    throw new UnsupportedOperationException("Item not found.");
   }
 
   // returns all items
