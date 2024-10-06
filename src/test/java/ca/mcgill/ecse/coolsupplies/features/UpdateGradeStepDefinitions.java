12cdfeac1ce7892f9ccf9f367ec04e07e27914b0
Trevor Piltch <trevor@piltch.com>
2024-10-06 12:07:17 -0400

    Add update steps

--- src/test/java/ca/mcgill/ecse/coolsupplies/features/UpdateGradeStepDefinitions.java
+++ src/test/java/ca/mcgill/ecse/coolsupplies/features/UpdateGradeStepDefinitions.java
@@ -1,9 +1,21 @@
 package ca.mcgill.ecse.coolsupplies.features;
 
+/* Project Imports */
+import ca.mcgill.ecse.coolsupplies.model.*;
+
+/* Cucumber Imports */
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
+/* JUnit Imports */
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+import java.util.ArrayList;
+
 public class UpdateGradeStepDefinitions {
+  private CoolSupplies coolSupplies;
+
   @When("the school admin attempts to update grade {string} in the system with level {string} \\(p13)")
   public void the_school_admin_attempts_to_update_grade_in_the_system_with_level_p13(String string,
       String string2) {
@@ -13,21 +25,26 @@ public class UpdateGradeStepDefinitions {
 
   @Then("the grade {string} shall not exist in the system \\(p13)")
   public void the_grade_shall_not_exist_in_the_system_p13(String string) {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
+    List<Grade> grades = coolSupplies.getGrades();
+
+    for (int i = 0; i < grades.size(); i++) {
+      assertTrue(grades.get(i).getLevel() != string);
+    }
   }
 
   @Then("the following grade entities shall exist in the system \\(p13)")
   public void the_following_grade_entities_shall_exist_in_the_system_p13(
       io.cucumber.datatable.DataTable dataTable) {
-    // Write code here that turns the phrase above into concrete actions
-    // For automatic transformation, change DataTable to one of
-    // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
-    // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
-    // Double, Byte, Short, Long, BigInteger or BigDecimal.
-    //
-    // For other transformations you can register a DataTableType.
-    throw new io.cucumber.java.PendingException();
-  }
 
+    List<String> grade_entities = dataTable.asList(String.class);
+    List<String> levels = new ArrayList<String>();
+    List<Grade> grades = coolSupplies.getGrades();
+
+    for (int i = 0; i < grades.size(); i++) {
+      levels.add(grades.get(i).getLevel());
+    }
+
+    assertTrue(levels.containsAll(grade_entities));
+  }
 }
+
