62b40c8a6a61992f7cadcfe9ea539326b77cab17
KennJos <kenny-alexander.joseph@mail.mcgill.ca>
2024-10-06 10:33:39 -0400

    updating code

--- src/main/java/ca/mcgill/ecse/coolsupplies/controller/CoolSuppliesFeatureSet7Controller.java
+++ src/main/java/ca/mcgill/ecse/coolsupplies/controller/CoolSuppliesFeatureSet7Controller.java
@@ -2,27 +2,44 @@ package ca.mcgill.ecse.coolsupplies.controller;
 
 import java.util.List;
 
+import ca.mcgill.ecse.coolsupplies.model.Grade;
+import ca.mcgill.ecse.coolsupplies.model.CoolSupplies;
+
 public class CoolSuppliesFeatureSet7Controller {
 
   public static String addGrade(String level) {
-    throw new UnsupportedOperationException("Not implemented yet.");
+    //implement code to find coolsuppliesapplication
+    Grade newgrade=new Grade(level,getCoolSupplies());
   }
 
   public static String updateGrade(String level, String newLevel) {
-    throw new UnsupportedOperationException("Not implemented yet.");
+    //Grade oldGrade = getWithLevel(level);
+    //boolean updated=oldgrade.setLevel(newLevel);
+    if (getWithLevel(level).setLevel(newLevel)) return "Grade updated successfully";
+    else return "Error: Grade was not updated";
   }
 
   public static String deleteGrade(String level) {
-    throw new UnsupportedOperationException("Not implemented yet.");
+
+    getWithLevel(level).delete();
+    List<Grade> allGrades = getGrades()
+    if (!hasWithlevel(level)) return "Grade successfully deleted";
+    else return "Error: Grade was not deleted";
+
   }
 
   public static TOGrade getGrade(String level) {
-    throw new UnsupportedOperationException("Not implemented yet.");
+    return TOGrade(level);
   }
 
   // returns all grades
   public static List<TOGrade> getGrades() {
-    throw new UnsupportedOperationException("Not implemented yet.");
+
+    List<TOGrade> grades;
+    //figure out how to access coolsupplies getgrades function
+    for (Grade i : CoolSupplies.getGrades())) grades.append(i);
+  return grades;
+
   }
 
 }
