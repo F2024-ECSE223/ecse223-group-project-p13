9ddbe7936d2b1296651dd5a7b6363b9968d1b6eb
Lune17 <luneletailleur@gmail.com>
2024-10-06 13:03:10 -0400

    Update updateAdmin function

--- src/main/java/ca/mcgill/ecse/coolsupplies/controller/CoolSuppliesFeatureSet1Controller.java
+++ src/main/java/ca/mcgill/ecse/coolsupplies/controller/CoolSuppliesFeatureSet1Controller.java
@@ -1,15 +1,21 @@
 package ca.mcgill.ecse.coolsupplies.controller;
 
 import java.util.List;
-
+import ca.mcgill.ecse.coolsupplies.model.*;
 public class CoolSuppliesFeatureSet1Controller {
-
+  private static CoolSupplies coolSupplies;
+  // can i create a static instance ?
   public static String updateAdmin(String password) {
-    throw new UnsupportedOperationException("Not implemented yet.");
+      // do I need to check if the old password is right
+      // need to call the setPassword() method on the admin instance not a new admin
+      coolSupplies.getAdmin().setPassword(password);
+      return "Admin password updated!";
   }
 
   public static String addParent(String email, String password, String name, int phoneNumber) {
-    throw new UnsupportedOperationException("Not implemented yet.");
+
+
+    return "New parent is registered!";
   }
 
   public static String updateParent(String email, String newPassword, String newName,
