2a1e94ecc9295441740f3d70a3abcbc6dfa21f88
KennJos <kenny-alexander.joseph@mail.mcgill.ca>
2024-10-06 12:04:35 -0400

    completed controller v1

--- src/main/java/ca/mcgill/ecse/coolsupplies/controller/CoolSuppliesFeatureSet7Controller.java
+++ src/main/java/ca/mcgill/ecse/coolsupplies/controller/CoolSuppliesFeatureSet7Controller.java
@@ -4,42 +4,38 @@ import java.util.List;
 
 import ca.mcgill.ecse.coolsupplies.model.Grade;
 import ca.mcgill.ecse.coolsupplies.model.CoolSupplies;
+import ca.mcgill.ecse.coolsupplies.application.CoolSuppliesApplication;
 
 public class CoolSuppliesFeatureSet7Controller {
 
   public static String addGrade(String level) {
     //implement code to find coolsuppliesapplication
-    Grade newgrade=new Grade(level,getCoolSupplies());
+    Grade newgrade = new Grade(level,CoolSuppliesApplication.getCoolSupplies);
+    if (!Grade.hasWithlevel(level)) return "Grade successfully added";
+    else return "Error: Grade was not added";
   }
 
   public static String updateGrade(String level, String newLevel) {
-    //Grade oldGrade = getWithLevel(level);
-    //boolean updated=oldgrade.setLevel(newLevel);
-    if (getWithLevel(level).setLevel(newLevel)) return "Grade updated successfully";
+    if (Grade.getWithLevel(level).setLevel(newLevel)) return "Grade updated successfully";
     else return "Error: Grade was not updated";
   }
 
   public static String deleteGrade(String level) {
-
     getWithLevel(level).delete();
-    List<Grade> allGrades = getGrades()
-    if (!hasWithlevel(level)) return "Grade successfully deleted";
+    if (!Grade.hasWithlevel(level)) return "Grade successfully deleted";
     else return "Error: Grade was not deleted";
 
   }
 
   public static TOGrade getGrade(String level) {
-    return TOGrade(level);
+    return TOGrade.TOGrade(level);
   }
 
   // returns all grades
   public static List<TOGrade> getGrades() {
-
     List<TOGrade> grades;
-    //figure out how to access coolsupplies getgrades function
-    for (Grade i : CoolSupplies.getGrades())) grades.append(i);
-  return grades;
-
+    for (Grade i : CoolSupplies.getGrades()) grades.add(getGrade(i.level));
+    return grades;
   }
 
 }
