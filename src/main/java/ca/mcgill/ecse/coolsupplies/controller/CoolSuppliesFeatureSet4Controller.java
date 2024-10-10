0e80e723ad421bc307dd79473ee4fc48feaffe66
Trevor Piltch <trevor@piltch.com>
2024-10-10 13:43:47 -0400

    Remove unfinished feature4

--- src/main/java/ca/mcgill/ecse/coolsupplies/controller/CoolSuppliesFeatureSet4Controller.java
+++ src/main/java/ca/mcgill/ecse/coolsupplies/controller/CoolSuppliesFeatureSet4Controller.java
@@ -9,33 +9,12 @@ public class CoolSuppliesFeatureSet4Controller {
     throw new UnsupportedOperationException("Not implemented yet.");
   }
 
-  public static String updateBundle(String name, String newName, int newDiscount,
-      String newGradeLevel) {
-    Grade myGrade = null;
-    for(int i = 0; i< CoolSuppliesApplication.getCoolSupplies().numberOfBundles(); i++) {
-      if(CoolSuppliesApplication.getCoolSupplies().getBundle(i).getName() == name){
-        GradeBundle myBundle= CoolSuppliesApplication.getCoolSupplies().getBundle(i);
-        boolean nameSet = myBundle.setName(newName);
-        boolean discountSet = myBundle.setDiscount(newDiscount);
-        for(int j=0; j<CoolSuppliesApplication.getCoolSupplies().numberOfGrades(); j++){
-          if (CoolSuppliesApplication.getCoolSupplies().getGrade(i).getLevel() == newGradeLevel){ //MAYBE SHOULD USE ISEQUALS
-            myGrade = CoolSuppliesApplication.getCoolSupplies().getGrade(i);
-          }
-        }
-        boolean gradeSet = myBundle.setGrade(myGrade);
-        if(nameSet && discountSet && gradeSet){
-          return "Bundle updated successfully.";
-        }
-        else {
-          return "Bundle was not updated successfully.";
-        }
-      }
-    }
-    return "Bundle was not found.";
+  public static String updateBundle(String name, String newName, int newDiscount, String newGradeLevel) {
+    throw new UnsupportedOperationException("Not implemented yet.");
   }
 
   public static String deleteBundle(String name) {
-    for(int i= 0; i< CoolSuppliesApplication.getCoolSupplies().)
+    throw new UnsupportedOperationException("Not implemented yet.");
   }
 
   public static TOGradeBundle getBundle(String name) {
