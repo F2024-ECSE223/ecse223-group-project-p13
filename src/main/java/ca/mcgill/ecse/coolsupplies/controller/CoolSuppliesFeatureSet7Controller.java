81ea312cef3ce3016f1dc5137bfb7d47c278e9fb
KennJos <kenny-alexander.joseph@mail.mcgill.ca>
2024-10-10 15:11:25 -0400

    updating code

--- src/main/java/ca/mcgill/ecse/coolsupplies/controller/CoolSuppliesFeatureSet7Controller.java
+++ src/main/java/ca/mcgill/ecse/coolsupplies/controller/CoolSuppliesFeatureSet7Controller.java
@@ -1,16 +1,16 @@
 package ca.mcgill.ecse.coolsupplies.controller;
 
 import java.util.List;
+import java.util.ArrayList;
 
 import ca.mcgill.ecse.coolsupplies.model.Grade;
-import ca.mcgill.ecse.coolsupplies.model.CoolSupplies;
 import ca.mcgill.ecse.coolsupplies.application.CoolSuppliesApplication;
 
 public class CoolSuppliesFeatureSet7Controller {
 
   public static String addGrade(String level) {
     //Initializes newgrade, then checks if initialization succeeded and displays status message accordingly
-    Grade newgrade = new Grade(level,CoolSuppliesApplication.getCoolSupplies);
+    new Grade(level,CoolSuppliesApplication.getCoolSupplies());
     if (!Grade.hasWithLevel(level)) return "Grade successfully added";
     else return "Error: Grade was not added";
   }
@@ -27,7 +27,7 @@ public class CoolSuppliesFeatureSet7Controller {
     //Calls delete level on the instance of grade
     //Checks if grade was successfully removed, then displays status message accordingly
     Grade.getWithLevel(level).delete();
-    if (!Grade.hasWithlevel(level)) return "Grade successfully deleted";
+    if (!Grade.hasWithLevel(level)) return "Grade successfully deleted";
     else return "Error: Grade was not deleted";
 
   }
@@ -40,8 +40,8 @@ public class CoolSuppliesFeatureSet7Controller {
   // returns all grades
   public static List<TOGrade> getGrades() {
     //Creates list of Grade TO's, iterates through list of all grades, then creates TO of each grade and adds it to the list
-    List<TOGrade> grades;
-    for (Grade i : CoolSupplies.getGrades()) grades.add(getGrade(i.level));
+    List<TOGrade> grades = new ArrayList<TOGrade>();
+    for (Grade i : CoolSuppliesApplication.getCoolSupplies().getGrades()) grades.add(getGrade(i.getLevel()));
     return grades;
   }
 
