14c95c7e33bbbc540ed6d1cd4e2dcf35ea66b34e
claradupuis <clarardupuis@gmail.com>
2024-10-13 13:21:02 -0400

    first 2 methods of feature2

--- src/main/java/ca/mcgill/ecse/coolsupplies/controller/CoolSuppliesFeatureSet2Controller.java
+++ src/main/java/ca/mcgill/ecse/coolsupplies/controller/CoolSuppliesFeatureSet2Controller.java
@@ -1,15 +1,40 @@
 package ca.mcgill.ecse.coolsupplies.controller;
 
+import ca.mcgill.ecse.coolsupplies.application.CoolSuppliesApplication;
+import ca.mcgill.ecse.coolsupplies.model.Grade;
+import ca.mcgill.ecse.coolsupplies.model.Student;
+
 import java.util.List;
 
 public class CoolSuppliesFeatureSet2Controller {
 
   public static String addStudent(String name, String gradeLevel) {
-    throw new UnsupportedOperationException("Not implemented yet.");
+
+
+    if (!name.isEmpty() || !gradeLevel.isEmpty() ) {
+      try {
+        Grade grade = new Grade(gradeLevel, CoolSuppliesApplication.getCoolSupplies());
+        new Student(name, CoolSuppliesApplication.getCoolSupplies(), grade);
+      } catch (RuntimeException e){
+
+      }
+
+    } else return "The name or the level cannot be empty or null.";
+
   }
 
   public static String updateStudent(String name, String newName, String newGradeLevel) {
-    throw new UnsupportedOperationException("Not implemented yet.");
+
+    if (!name.isEmpty() || !newGradeLevel.isEmpty() || !newName.isEmpty() ) {
+      if (Student.getWithName(name) != null){
+        if (!Student.getWithName(name).setName(newName)){
+          return "what";
+        }
+        return "";
+        }
+
+      }
+      else return "The name or the level cannot be empty or null.";
   }
 
   public static String deleteStudent(String name) {
