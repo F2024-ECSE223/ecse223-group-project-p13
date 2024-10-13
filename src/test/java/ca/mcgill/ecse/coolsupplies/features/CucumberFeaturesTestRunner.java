61916cb8a879288e7c0197c06485e3f8f2f6d10b
Trevor Piltch <trevor@piltch.com>
2024-10-13 15:28:04 -0400

    Update fixes from shared branch and update test runner

--- src/test/java/ca/mcgill/ecse/coolsupplies/features/CucumberFeaturesTestRunner.java
+++ src/test/java/ca/mcgill/ecse/coolsupplies/features/CucumberFeaturesTestRunner.java
@@ -5,7 +5,11 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = "pretty", features = "src/test/resources/AddGrade.feature",
+@CucumberOptions(plugin = "pretty", features = 
+  {
+    "src/test/resources/AddGrade.feature",
+    "src/test/resources/UpdateGrade.feature"
+    },
     glue = "ca.mcgill.ecse.coolsupplies.features")
 public class CucumberFeaturesTestRunner {
 
--- src/test/java/ca/mcgill/ecse/coolsupplies/features/UpdateGradeStepDefinitions.java
+++ src/test/java/ca/mcgill/ecse/coolsupplies/features/UpdateGradeStepDefinitions.java
@@ -1,3 +1,4 @@
+
 package ca.mcgill.ecse.coolsupplies.features;
 
 /* Project Imports */
@@ -8,10 +9,11 @@ import ca.mcgill.ecse.coolsupplies.controller.CoolSuppliesFeatureSet7Controller;
 /* Cucumber Imports */
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import io.cucumber.java.en.Given;
 
 /* JUnit Imports */
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 /* Helper Imports */
@@ -24,25 +26,25 @@ public class UpdateGradeStepDefinitions {
 
   /**
    * @author Kenny-Alexander Joseph
-   * @param string the level that is going to be updated in the system
-   * @param string2 the new level that the old level is going to be updated to in the system
+   * @param level the level that is going to be updated in the system
+   * @param updatedLevel the new level that the old level is going to be updated to in the system
    * @return void
    * This gherkin step verifies that the first given level is updated with the second given level.
    */
   @When("the school admin attempts to update grade {string} in the system with level {string} \\(p13)")
-  public void the_school_admin_attempts_to_update_grade_in_the_system_with_level_p13(String string,
-      String string2) {
-    callController(CoolSuppliesFeatureSet7Controller.updateGrade(string,string2));
+  public void the_school_admin_attempts_to_update_grade_in_the_system_with_level_p13(String level,
+      String updatedLevel) {
+    callController(CoolSuppliesFeatureSet7Controller.updateGrade(level,updatedLevel));
   }
 
   /**
    * @author Trevor Piltch
-   * @param string the level which should not exist in the system
+   * @param level the level which should not exist in the system
    * @return void
    * This gherkin step verifies that the given level does not exist in the system.
    */
   @Then("the grade {string} shall not exist in the system \\(p13)")
-  public void the_grade_shall_not_exist_in_the_system_p13(String string) {
+  public void the_grade_shall_not_exist_in_the_system_p13(String level) {
     List<Grade> grades = coolSupplies.getGrades();
     List<String> levels = new ArrayList<>();
 
@@ -50,23 +52,23 @@ public class UpdateGradeStepDefinitions {
       levels.add(grades.get(i).getLevel());
     }
 
-    assertFalse(levels.contains(string));
+    assertFalse("Expected levels to not contain " + level, levels.contains(level));
   }
 
   /**
    * @author Trevor Piltch
-   * @param dataTable the list of grades that should exist in the system
+   * @param grades the list of grades that should exist in the system
    * @return void
    * This gherkin step verifies that the list of grades exists in the system
    */
   @Then("the following grade entities shall exist in the system \\(p13)")
   public void the_following_grade_entities_shall_exist_in_the_system_p13(
-      io.cucumber.datatable.DataTable dataTable) {
+      io.cucumber.datatable.DataTable grades) {
 
-    List<Map<String, String>> entities = dataTable.asMaps();
+    List<Map<String, String>> entities = grades.asMaps();
     List<String> levels = new ArrayList<>();
 
-    List<String> system_levels = new ArrayList<>();
+    List<String> systemLevels = new ArrayList<>();
 
     for (var entity : entities) {
       String level = entity.get("level");
@@ -74,10 +76,11 @@ public class UpdateGradeStepDefinitions {
     }
 
     for (var grade : coolSupplies.getGrades()) {
-      system_levels.add(grade.getLevel());
+      systemLevels.add(grade.getLevel());
     }
 
-    assertTrue(system_levels.containsAll(levels));
+    assertTrue("Expected levels: " + levels + " to exist in the system",
+        systemLevels.containsAll(levels));
   }
 
   /* Helper Methods */
@@ -94,4 +97,5 @@ public class UpdateGradeStepDefinitions {
     }
   }
 }
+
 
