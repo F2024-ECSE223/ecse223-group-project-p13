30cd0b47409e56eee00d05030bd92b9e7b28f588
claradupuis <clarardupuis@gmail.com>
2024-10-16 14:54:04 -0400

    adding completed controller for feature 2

--- src/main/java/ca/mcgill/ecse/coolsupplies/controller/CoolSuppliesFeatureSet2Controller.java
+++ src/main/java/ca/mcgill/ecse/coolsupplies/controller/CoolSuppliesFeatureSet2Controller.java
@@ -4,6 +4,7 @@ import ca.mcgill.ecse.coolsupplies.application.CoolSuppliesApplication;
 import ca.mcgill.ecse.coolsupplies.model.Grade;
 import ca.mcgill.ecse.coolsupplies.model.Student;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CoolSuppliesFeatureSet2Controller {
@@ -11,13 +12,14 @@ public class CoolSuppliesFeatureSet2Controller {
   public static String addStudent(String name, String gradeLevel) {
 
 
-    if (!name.isEmpty() || !gradeLevel.isEmpty() ) {
+    if (!(name == null) && !(name.equals("") && !(gradeLevel == null)&&!(gradeLevel.equals(""))) ) {
       try {
         Grade grade = new Grade(gradeLevel, CoolSuppliesApplication.getCoolSupplies());
         new Student(name, CoolSuppliesApplication.getCoolSupplies(), grade);
       } catch (RuntimeException e){
 
       }
+      return "";
 
     } else return "The name or the level cannot be empty or null.";
 
@@ -25,29 +27,41 @@ public class CoolSuppliesFeatureSet2Controller {
 
   public static String updateStudent(String name, String newName, String newGradeLevel) {
 
-    if (!name.isEmpty() || !newGradeLevel.isEmpty() || !newName.isEmpty() ) {
-      if (Student.getWithName(name) != null){
-        if (!Student.getWithName(name).setName(newName)){
-          return "what";
-        }
+    if (!name.isEmpty() && !newGradeLevel.isEmpty() && !newName.isEmpty() && !(name == null) && !(newName == null) && !(newGradeLevel == null)) {
+      if (Student.getWithName(name) != null) {
+        if (!Student.getWithName(name).setName(newName)) return "The student's name could not be changed.";
+        if (!Student.getWithName(name).setGrade(new Grade(newGradeLevel, CoolSuppliesApplication.getCoolSupplies()))) return "The student's garde could not be modified.";
         return "";
-        }
 
-      }
-      else return "The name or the level cannot be empty or null.";
+      }else return "the Student does not exist.";
+
+    }
+    else return "The name or the level cannot be empty or null.";
   }
 
   public static String deleteStudent(String name) {
-    throw new UnsupportedOperationException("Not implemented yet.");
+
+    if (!name.equals("") && !(name == null)){
+      if (Student.getWithName(name) != null){
+        Student.getWithName(name).delete();
+        return "";
+      }
+      else return "The Student could not be found.";
+    } else return "The name of the student cannot be empty or null.";
   }
 
   public static TOStudent getStudent(String name) {
-    throw new UnsupportedOperationException("Not implemented yet.");
+    return new TOStudent(name, Student.getWithName(name).getGrade().getLevel());
   }
 
   // returns all students
   public static List<TOStudent> getStudents() {
-    throw new UnsupportedOperationException("Not implemented yet.");
+    List<TOStudent> students = new ArrayList<>();
+    for (Student s : CoolSuppliesApplication.getCoolSupplies().getStudents()) {
+      students.add(getStudent(s.getName()));
+    }
+      return students;
+
   }
 
 }
