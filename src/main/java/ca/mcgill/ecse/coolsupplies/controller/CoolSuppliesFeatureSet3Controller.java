1f8fc8a0050a321c81989431150d5b4a6bf094a2
Dimitri Christopoulos <154035020+DimitriC3@users.noreply.github.com>
2024-10-16 21:58:40 -0400

    version 1, TODO: errors, JavaDoc, testing

--- src/main/java/ca/mcgill/ecse/coolsupplies/controller/CoolSuppliesFeatureSet3Controller.java
+++ src/main/java/ca/mcgill/ecse/coolsupplies/controller/CoolSuppliesFeatureSet3Controller.java
@@ -1,64 +1,93 @@
 package ca.mcgill.ecse.coolsupplies.controller;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-// Model Import
-import ca.mcgill.ecse.coolsupplies.model.*;
+// Imports
 
+import ca.mcgill.ecse.coolsupplies.model.*;
+import ca.mcgill.ecse.coolsupplies.application.CoolSuppliesApplication;
+
+// Feature Set 3
+
+// TODO: JavaDoc
 
 public class CoolSuppliesFeatureSet3Controller {
 
-  private static CoolSupplies coolSupplies;
+  private static final CoolSupplies coolSupplies = CoolSuppliesApplication.getCoolSupplies();
   private static List<Item> items;
 
   public static String addItem(String name, int price) {
     
-    // Create new item
-    Item new_item = new Item(name, price, coolSupplies);
-    items.add(new_item);
+    
+    try {
 
+      // Create new item
+      Item new_item = new Item(name, price, coolSupplies);
+      coolSupplies.addItem(new_item);
+    } 
+
+    catch (Exception e) {  // TODO: Fix return strings
+
+      //Check if new_item already exists
+      if (e.getMessage().equals("change this message")) {
+        return "Item already exists";
+      }
+
+      // Other errors (invalid arguments)
+      else {
+        return "Invalid argument(s)";
+      }
+    }
+
+    return "";  // Success message
+    
   }
 
   public static String updateItem(String name, String newName, int newPrice) {
     
-    List<Item> items = coolSupplies.getItems();
-
-    for (Item item : items) {
-      if (item.getName().equals(name)) {
-        item.setName(newName);
-        item.setPrice(newPrice);
-      }
-    }
-
+    // TODO: Handle errors
+    // Get Item then set new name and new price
+    Item item = (Item) Item.getWithName(name);
+    item.setName(newName);
+    item.setPrice(newPrice);
+    return "";
   }
 
   public static String deleteItem(String name) {
 
-    List<Item> items = coolSupplies.getItems();
+    // Check if list of items is empty
+    if (coolSupplies.hasItems()) {
 
-    for (Item item : items) {
-      if (item.getName().equals(name)) {
-        item = null;
-      }
+      // If list is not empty, get item and delete it
+      Item item = (Item) Item.getWithName(name);
+      item.delete();
+
+      return "";
     }
+    else return "Item list is empty";
   }
 
   public static TOItem getItem(String name) {
 
-    List<Item> items = coolSupplies.getItems();
+    Item item = (Item) Item.getWithName(name);
+    TOItem to_item = new TOItem(item.getName(), item.getPrice());
+    return to_item;
 
-    for (Item item : items) {
-      if (item.getName().equals(name)) {
-        TOItem to_item = new TOItem(item.getName(), item.getPrice());
-        return to_item;
-      }
-    }
-    throw new UnsupportedOperationException("Item not found.");
   }
 
   // returns all items
   public static List<TOItem> getItems() {
-    throw new UnsupportedOperationException("Not implemented yet.");
+
+    List<TOItem> toitem_list = new ArrayList<>();  // Tranfer object item list
+    List<Item> item_list = coolSupplies.getItems();  // List of current objects
+
+    // Iterate over current objects and add them to the new list by creating TOItem objects
+    for (Item item : item_list) {
+      toitem_list.add(getItem(item.getName()));
+    }
+    return toitem_list;
   }
 
 }
