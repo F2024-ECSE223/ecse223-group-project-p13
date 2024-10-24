bf837986b8b935968c7219cf4a2824bceacf82a2
claradupuis <clarardupuis@gmail.com>
2024-10-24 16:00:46 -0400

    changing format

--- src/main/java/ca/mcgill/ecse/coolsupplies/controller/CoolSuppliesFeatureSet2Controller.java
+++ src/main/java/ca/mcgill/ecse/coolsupplies/controller/CoolSuppliesFeatureSet2Controller.java
@@ -17,8 +17,7 @@ public class CoolSuppliesFeatureSet2Controller {
    * This method attempts to add a student
    */
   public static String addStudent(String name, String gradeLevel) {
-
-
+    
     if (name.equals("")) {
       return "The name must not be empty.";
 
