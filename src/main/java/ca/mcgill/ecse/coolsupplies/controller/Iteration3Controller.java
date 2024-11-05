5a3ea923e13ba7917e5252f792edc775c98e94ca
Trevor Piltch <trevor@piltch.com>
2024-11-05 16:52:00 -0500

    Add controller methods

---
+++ src/main/java/ca/mcgill/ecse/coolsupplies/controller/Iteration3Controller.java
@@ -0,0 +1,49 @@
+package ca.mcgill.ecse.coolsupplies.controller;
+
+/* Project Imports */
+import ca.mcgill.ecse.coolsupplies.application.CoolSuppliesApplication;
+import ca.mcgill.ecse.coolsupplies.model.*;
+
+public class Iteration3Controller {
+  private static CoolSupplies coolSupplies = CoolSuppliesApplication.getCoolSupplies();
+
+  public static String updateOrder(String newLevel, String student) {
+    throw new UnsupportedOperationException("Not Implemented yet.");
+  }
+
+  public static String addItem(String item, String quantity, String orderNumber) {
+    throw new UnsupportedOperationException("Not Implemented yet.");
+  }
+
+  public static String updateOrderQuantity(String item, String quantity) {
+    throw new UnsupportedOperationException("Not Implemented yet.");
+  }
+
+  public static String deleteItem(String item) {
+    throw new UnsupportedOperationException("Not Implemented yet.");
+  }
+
+  public static String payForOrder() {
+    throw new UnsupportedOperationException("Not Implemented yet.");
+  }
+
+  public static String payPenaltyForOrder() {
+    throw new UnsupportedOperationException("Not Implemented yet.");
+  }
+
+  public static String cancelOrder() {
+    throw new UnsupportedOperationException("Not Implemented yet.");
+  }
+
+  public static String viewAllOrders() {
+    throw new UnsupportedOperationException("Not Implemented yet.");
+  }
+
+  public static String viewOrder(String orderNumber) {
+    throw new UnsupportedOperationException("Not Implemented yet.");
+  }
+
+  public static String startSchoolYear() {
+    throw new UnsupportedOperationException("Not Implemented yet.");
+  }
+}
