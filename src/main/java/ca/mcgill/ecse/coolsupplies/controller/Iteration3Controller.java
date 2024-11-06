bafc09b7c0a6285a887545c791b06c9eb76ef2f6
nil-akkurt <nil.akkurt@mail.mcgill.ca>
2024-11-06 16:50:42 -0500

    adding assigned feature and step definitions

--- src/main/java/ca/mcgill/ecse/coolsupplies/controller/Iteration3Controller.java
+++ src/main/java/ca/mcgill/ecse/coolsupplies/controller/Iteration3Controller.java
@@ -3,6 +3,7 @@ package ca.mcgill.ecse.coolsupplies.controller;
 /* Project Imports */
 import ca.mcgill.ecse.coolsupplies.application.CoolSuppliesApplication;
 import ca.mcgill.ecse.coolsupplies.model.*;
+import java.util.*;
 
 public class Iteration3Controller {
   private static CoolSupplies coolSupplies = CoolSuppliesApplication.getCoolSupplies();
@@ -19,9 +20,63 @@ public class Iteration3Controller {
     throw new UnsupportedOperationException("Not Implemented yet.");
   }
 
+  /**
+   * Deletes an item from a specific order if the entered item is valid, exists in the order,
+   * the order exists, and is still in the Started status. If not, returns the appropriate error message
+   * @author Nil Akkurt
+   * @param item
+   * @param orderNumber
+   * @return String
+   */
   public static String deleteItem(String item, String orderNumber) {
-    throw new UnsupportedOperationException("Not Implemented yet.");
+    int orderNum = Integer.parseInt(orderNumber);
+    Order myOrder = Order.getWithNumber(orderNum);
+    List<OrderItem> items = myOrder.getOrderItems();
+    InventoryItem itemToDel = null;
+    OrderItem orItemToDel = null;
+    List<OrderItem> itemsInSystem = CoolSuppliesApplication.getCoolSupplies().getOrderItems();
+    InventoryItem inItem = null;
+    for(OrderItem orItem: itemsInSystem) {
+      if(orItem.getItem().getName().equals(item)){
+        inItem = orItem.getItem();
+      }
+    }
+    if(inItem == null) {
+      return "Item" + item + "does not exist.";
+    }
+
+    for(OrderItem myItem: items){
+      String itemName = myItem.getItem().getName();
+      if(item.equals(itemName)){
+        itemToDel = myItem.getItem();
+        orItemToDel = myItem;
+      }
+    }
+    if(itemToDel == null){
+      return "Item" + item +"does not exist in order" + orderNumber + ".";
+    }
+
+    for
+    if(myOrder == null){
+      return "Order" + orderNumber + "does not exist";
+    }
+    if(myOrder.getStatusFullName().equals("Paid")){
+      return "Cannot delete items from a paid order";
+    }
+    if(myOrder.getStatusFullName().equals("Penalized")){
+      return "Cannot delete items from a penalized order";
+    }
+    if(myOrder.getStatusFullName().equals("Prepared")){
+      return "Cannot delete items from a prepared order";
+    }
+    if(myOrder.getStatusFullName().equals("PickedUp")){
+      return "Cannot delete items from a picked up order";
+    }
+    myOrder.removeOrderItem(orItemToDel);
+    return "";
   }
+
+
 
   public static String payForOrder(String orderNumber) { 
     throw new UnsupportedOperationException("Not Implemented yet.");
