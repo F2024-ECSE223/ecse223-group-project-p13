bb0566f34d8ed539bd99c9e94fe4c31aa5e0e128
Trevor Piltch <trevor@piltch.com>
2024-11-06 14:05:43 -0500

    Added preliminary payPenalty method

--- src/main/java/ca/mcgill/ecse/coolsupplies/controller/Iteration3Controller.java
+++ src/main/java/ca/mcgill/ecse/coolsupplies/controller/Iteration3Controller.java
@@ -4,6 +4,9 @@ package ca.mcgill.ecse.coolsupplies.controller;
 import ca.mcgill.ecse.coolsupplies.application.CoolSuppliesApplication;
 import ca.mcgill.ecse.coolsupplies.model.*;
 
+/* Util Imports */
+import java.util.List;
+
 public class Iteration3Controller {
   private static CoolSupplies coolSupplies = CoolSuppliesApplication.getCoolSupplies();
 
@@ -27,8 +30,33 @@ public class Iteration3Controller {
     throw new UnsupportedOperationException("Not Implemented yet.");
   }
 
-  public static String payPenaltyForOrder(String orderNumber) {
-    throw new UnsupportedOperationException("Not Implemented yet.");
+  public static String payPenaltyForOrder(String orderNumber, String authCode, String penaltyAuthCode) {
+    Integer num = Integer.parseInt(orderNumber);
+    Order order = Order.getWithNumber(num);
+
+    if (order == null) {
+      return "Order " + orderNumber + " does not exist";
+    }
+
+    /* 
+    if (order.getStatus() != Prepared) {
+      return "Cannot pay penalty for a " + order.getStatus() + " order"
+    }
+    */
+
+    if (authCode.isEmpty()) {
+      return "Authorization code is invalid.";
+    }
+
+    if (penaltyAuthCode.isEmpty()) {
+      return "Penalty authorization code is invalid";
+    }
+
+    /*
+     * order.payPenalty(authCode, panaltyCode)
+     */
+
+    return "";
   }
 
   public static String pickUpOrder(String orderNumber) {
