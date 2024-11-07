4ae6c21bb7ffcf12ccf9aa6b8952dae1b077f909
Dimitri Christopoulos <154035020+DimitriC3@users.noreply.github.com>
2024-11-07 16:59:41 -0500

    change controller method

--- src/main/java/ca/mcgill/ecse/coolsupplies/controller/Iteration3Controller.java
+++ src/main/java/ca/mcgill/ecse/coolsupplies/controller/Iteration3Controller.java
@@ -1,8 +1,12 @@
 package ca.mcgill.ecse.coolsupplies.controller;
 
 /* Project Imports */
+import java.util.List;
+
 import ca.mcgill.ecse.coolsupplies.application.CoolSuppliesApplication;
-import ca.mcgill.ecse.coolsupplies.model.*;
+import ca.mcgill.ecse.coolsupplies.model.CoolSupplies;
+import ca.mcgill.ecse.coolsupplies.model.Order;
+import ca.mcgill.ecse.coolsupplies.model.OrderItem;
 
 public class Iteration3Controller {
   private static CoolSupplies coolSupplies = CoolSuppliesApplication.getCoolSupplies();
@@ -16,7 +20,59 @@ public class Iteration3Controller {
   }
 
   public static String updateOrderQuantity(String item, String quantity, String orderNumber) {
-    throw new UnsupportedOperationException("Not Implemented yet.");
+
+    // Obtain the order object
+    int orderNumberInt = Integer.parseInt(orderNumber);
+
+    List<Order> orderList = coolSupplies.getOrders();
+    Order order = null;
+
+    // Find order from the order in the system
+    for (Order orderFromList : orderList) {
+      if (orderFromList.getNumber() == orderNumberInt) {
+        order = orderFromList;
+      }
+    }
+
+    // Error if orderNumber is not in system
+    if (order == null) {
+	    return "Order " + orderNumberInt + " does not exist";
+    }
+
+    for (OrderItem orderItem : order.getOrderItems()) {}
+    
+    for (OrderItem orderItem : order.getOrderItems()) {
+
+      // Successfully update item
+      if ((orderItem.getItem().getName()).equals(item)) {
+        orderItem.setQuantity(Integer.parseInt(quantity));
+        return "";
+      }
+      // Item not found in system
+      else {
+        return "Item " + item + " does not exist in the order " + orderNumber + ".";
+      }
+    }
+
+    // Quantity must be greater than 0
+    if (Integer.parseInt(quantity) < 1) {
+      return "Quantity must be greater than 0.";
+    }
+
+    // Error if status is not started
+    switch (order.getStatus()) {
+        case Paid:
+            return "Cannot update items in a paid order";
+        case Penalized:
+            return "Cannot update items in a penalized order";
+        case Prepared:
+            return "Cannot update items in a prepared order";
+        case PickedUp:
+            return "Cannot update items in a picked up order";
+        default:
+            break;
+    }
+    return "Item " + item + " does not exist.";
   }
 
   public static String deleteItem(String item, String orderNumber) {
