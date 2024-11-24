9a043ce04dfd0b7d3c2367cbca1bc31a59845a1a
Trevor Piltch <trevor@piltch.com>
2024-11-24 10:10:09 -0500

    Add sidebar

--- src/main/java/ca/mcgill/ecse/coolsupplies/application/CoolSuppliesApplication.java
+++ src/main/java/ca/mcgill/ecse/coolsupplies/application/CoolSuppliesApplication.java
@@ -2,13 +2,15 @@ package ca.mcgill.ecse.coolsupplies.application;
 
 import ca.mcgill.ecse.coolsupplies.model.CoolSupplies;
 import ca.mcgill.ecse.coolsupplies.persistence.CoolSuppliesPersistence;
+import ca.mcgill.ecse.coolsupplies.view.CoolSuppliesFxmlView;
+import javafx.application.Application;
 
 public class CoolSuppliesApplication {
 
   private static CoolSupplies coolSupplies;
 
   public static void main(String[] args) {
-    // TODO Start the application user interface here
+    Application.launch(CoolSuppliesFxmlView.class, args);
   }
 
   public static CoolSupplies getCoolSupplies() {
---
+++ src/main/java/ca/mcgill/ecse/coolsupplies/view/CoolSuppliesFxmlView.java
@@ -0,0 +1,150 @@
+package ca.mcgill.ecse.coolsupplies.view;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import atlantafx.base.theme.PrimerLight;
+import javafx.application.Application;
+import javafx.event.Event;
+import javafx.event.EventType;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class CoolSuppliesFxmlView extends Application {
+  public static final EventType<Event> REFRESH_EVENT = new EventType<>("REFRESH");
+  private static CoolSuppliesFxmlView instance;
+  private List<Node> refreshableNodes = new ArrayList<>();
+
+  private StackPane mainContent;
+
+  @Override
+  public void start(Stage primaryStage) {
+    Application.setUserAgentStylesheet(new PrimerLight().getUserAgentStylesheet());
+    BorderPane root = new BorderPane();
+
+    VBox sidebar = createSidebar();
+    sidebar.setPrefWidth(200);
+
+    mainContent = new StackPane();
+
+    updateContent("Home");
+
+    root.setLeft(sidebar);
+    root.setCenter(mainContent);
+
+    Scene scene = new Scene(root, 800, 600);
+
+    primaryStage.setMinHeight(600);
+    primaryStage.setMinWidth(800);
+    primaryStage.setTitle("CoolSupplies");
+    primaryStage.setScene(scene);
+    primaryStage.show();
+  }
+
+  private VBox createSidebar() {
+    VBox sidebar = new VBox(10);
+    sidebar.setPadding(new Insets(10));
+    sidebar.setAlignment(Pos.TOP_CENTER);
+
+    HBox header = createHeader();
+
+    VBox navigation = new VBox(10);
+    String[] pages = {"Home", "Manage", "Settings", "Help"};
+
+    for (String page : pages) {
+      Button button = createNavButton(page);
+      navigation.getChildren().add(button);
+    }
+
+    sidebar.getChildren().addAll(header, navigation);
+
+    return sidebar;
+  }
+
+  private HBox createHeader() {
+    HBox header = new HBox(10);
+    header.setPadding(new Insets(15));
+    header.setAlignment(Pos.CENTER_LEFT);
+
+    Text title = new Text("CoolSupplies");
+    ImageView imageView = new ImageView();
+    Image icon = new Image(CoolSuppliesFxmlView.class.getResourceAsStream("resources/icon.png"));
+    imageView.setImage(icon);
+    imageView.setFitHeight(60);
+    imageView.setFitWidth(60);
+    imageView.setPreserveRatio(true);
+
+    header.getChildren().addAll(imageView, title);
+
+    return header;
+  }
+
+  private Button createNavButton(String text) {
+    Button button = new Button(text);
+    button.setMaxWidth(Double.MAX_VALUE);
+    button.setAlignment(Pos.CENTER_LEFT);
+    button.setPadding(new Insets(10));
+
+    button.setOnAction(e -> updateContent(text));
+
+    return button;
+  }
+
+  private void updateContent(String page) {
+    mainContent.getChildren().clear();
+
+    if (page.toLowerCase().equals("manage")) {
+      try {
+        var root = (Pane) FXMLLoader.load(getClass().getResource("ParentManager.fxml"));
+
+        mainContent.getChildren().add(root);
+        return;
+      } catch (IOException e) {
+        System.out.println(e);
+      }
+    } else {
+      StackPane content = new StackPane();
+      Text text = new Text("This is the " + page + " page");
+      text.setStyle("-fx-font-size: 24px;");
+      content.getChildren().add(text);
+      mainContent.getChildren().add(content);
+    }
+  }
+
+  public void registerRefreshEvent(Node node) {
+    refreshableNodes.add(node);
+  }
+
+  public void registerRefreshEvent(Node... nodes) {
+    for (var node : nodes) {
+      refreshableNodes.add(node);
+    }
+  }
+
+  public void removeRefreshableNode(Node node) {
+    refreshableNodes.remove(node);
+  }
+
+  public void refresh() {
+    for (Node node : refreshableNodes) {
+      node.fireEvent(new Event(REFRESH_EVENT));
+    }
+  }
+
+  public static CoolSuppliesFxmlView getInstance() {
+    return instance;
+  }
+}
