875fa13d51267c7d324148c7003cb1298dfd434f
Lune17 <luneletailleur@gmail.com>
2024-11-27 12:49:40 -0500

    update password

--- src/main/java/ca/mcgill/ecse/coolsupplies/view/UpdateAdminView.java
+++ src/main/java/ca/mcgill/ecse/coolsupplies/view/UpdateAdminView.java
@@ -1,2 +1,36 @@
-package ca.mcgill.ecse.coolsupplies.view;public class UpdateAdminView {
+package ca.mcgill.ecse.coolsupplies.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+import ca.mcgill.ecse.coolsupplies.application.CoolSuppliesApplication;
+import ca.mcgill.ecse.coolsupplies.controller.CoolSuppliesFeatureSet1Controller;
+
+import java.util.Objects;
+
+public class UpdateAdminView {
+
+    @FXML
+    private Button buttonSaveUpdateAdmin;
+
+    @FXML
+    private PasswordField newPasswordUpdateAdmin;
+
+    @FXML
+    private Label errorMessage;
+
+
+    @FXML
+    public void saveButtonClickedUpdateAdmin (){
+       String  newPassword = newPasswordUpdateAdmin.getText();
+       String message = CoolSuppliesFeatureSet1Controller.updateAdmin(newPassword);
+       if (message.isEmpty())
+       {
+           errorMessage.setText(message);
+       }
+
+
+       // ADD ERROR LABEL
+    }
+
 }
