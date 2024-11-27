f65ae8f7d4377a5fb15223839846456a68d71020
Lune17 <luneletailleur@gmail.com>
2024-11-27 13:46:21 -0500

    register parent view

---
+++ src/main/java/ca/mcgill/ecse/coolsupplies/view/RegisterParentView.java
@@ -0,0 +1,47 @@
+package ca.mcgill.ecse.coolsupplies.view;
+
+import ca.mcgill.ecse.coolsupplies.application.CoolSuppliesApplication;
+import ca.mcgill.ecse.coolsupplies.controller.CoolSuppliesFeatureSet1Controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+import java.awt.event.ActionEvent;
+
+public class RegisterParentView {
+    @FXML
+    private TextField emailRegisterParent;
+
+    @FXML
+    private TextField nameRegisterParent;
+
+    @FXML
+    private TextField phoneRegisterParent;
+
+    @FXML
+    private TextField passwordRegisterParent;
+
+    @FXML
+    private Button buttonSaveRegisterParent;
+
+    @FXML
+    private Label errorMessageRegisterParent;
+
+
+    @FXML
+    public void saveButtonClickedRegisterParent (ActionEvent event)
+    {
+        String email = emailRegisterParent.getText();
+        String phoneNumberString = phoneRegisterParent.getText();
+        int phoneNumber = Integer.parseInt(phoneNumberString);
+        String name = nameRegisterParent.getText();
+        String password = passwordRegisterParent.getText();
+
+        String message = CoolSuppliesFeatureSet1Controller.addParent(email, password, name, phoneNumber);
+        if (message.isEmpty())
+        {
+            errorMessageRegisterParent.setText(message);
+        }
+    }
+
+}
