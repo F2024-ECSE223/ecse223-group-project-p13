fa7053f206d1e0afba9e58a02a2c363d02e1efdc
Dimitri Christopoulos <154035020+DimitriC3@users.noreply.github.com>
2024-11-29 00:33:40 -0500

    button click and error popup

--- src/main/java/ca/mcgill/ecse/coolsupplies/view/ItemViewController.java
+++ src/main/java/ca/mcgill/ecse/coolsupplies/view/ItemViewController.java
@@ -3,28 +3,37 @@ package ca.mcgill.ecse.coolsupplies.view;
 import javax.swing.table.TableColumn;
 
 import ca.mcgill.ecse.coolsupplies.controller.TOItem;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import ca.mcgill.ecse.coolsupplies.controller.CoolSuppliesFeatureSet3Controller;
 
 public class ItemViewController {
 
   @FXML
-  private TextField item_item_name_label; 
+  private TextField new_item_name_text_field; 
 
   @FXML
-  private TextField item_price_label;
+  private TextField new_price_text_field;
 
   @FXML
-  private Button item_add_item_button;
+  private Button add_item_button;
 
   @FXML
   private TableView<TOItem> item_table_view;
 
   @FXML
-  private TableColumn item_item_column;
+  private TableColumn item_name_column;
 
   @FXML
   private TableColumn item_price_column;
@@ -34,9 +43,64 @@ public class ItemViewController {
 
   private ObservableList<TOItem> itemList;
 
-  public void start() {
-    
+  @FXML
+  public void initializeTable() {
+    itemList = FXCollections.observableArrayList();
   }
+
+  @FXML
+  public void addItemClicked(ActionEvent event) {
+
+    String newName = new_item_name_text_field.getText();
+    String newPrice = new_price_text_field.getText();
+
+    if (newName == null || newName.trim().isEmpty()) {
+      showError("Please input a valid item name");
+    } 
+    else if (newPrice == null || newPrice.trim().isEmpty()) {
+      showError("Please input a valid item price");
+    }
+    String addMessage = CoolSuppliesFeatureSet3Controller.addItem(newName, Integer.parseInt(newPrice));
+    if (addMessage.isEmpty()) {
+      new_item_name_text_field.setText("");
+      new_price_text_field.setText("");
+    }
+    else {
+      showError(addMessage);
+    }
+  }
+
+
+  // Helper methods
+
+  // Error popup from tutorial
+  private  static void makePopupWindow(String title, String message) {
+    Stage dialog = new Stage();
+    dialog.initModality(Modality.APPLICATION_MODAL);
+    VBox dialogPane = new VBox();
+
+    // create UI elements
+    Text text = new Text(message);
+    Button okButton = new Button("OK");
+    okButton.setOnAction(a -> dialog.close());
+
+    // display the popup window
+    int innerPadding = 10; // inner padding/spacing
+    int outerPadding = 100; // outer padding
+    dialogPane.setSpacing(innerPadding);
+    dialogPane.setAlignment(Pos.CENTER);
+    dialogPane.setPadding(new Insets(innerPadding, innerPadding, innerPadding, innerPadding));
+    dialogPane.getChildren().addAll(text, okButton);
+    Scene dialogScene = new Scene(dialogPane, outerPadding + 5 * message.length(), outerPadding);
+    dialog.setScene(dialogScene);
+    dialog.setTitle(title);
+    dialog.show();
+  }
+
+  private static void showError(String message) {
+    makePopupWindow("Error", message);
+  }
+
 
   
   
