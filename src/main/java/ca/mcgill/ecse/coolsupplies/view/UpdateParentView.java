b6423525da7a1e8b2bf981c0a1522df0dfaf1340
Lune17 <luneletailleur@gmail.com>
2024-11-30 13:31:54 -0500

    can update parent

--- src/main/java/ca/mcgill/ecse/coolsupplies/view/UpdateParentView.java
+++ src/main/java/ca/mcgill/ecse/coolsupplies/view/UpdateParentView.java
@@ -1,16 +1,20 @@
 package ca.mcgill.ecse.coolsupplies.view;
 
+import java.util.ArrayList;
+import java.util.List;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 
 import ca.mcgill.ecse.coolsupplies.controller.CoolSuppliesFeatureSet1Controller;
 
 import java.awt.event.ActionEvent;
+import ca.mcgill.ecse.coolsupplies.controller.TOParent;
 
 public class UpdateParentView {
 
     @FXML
-    private TextField emailUpdateParent;
+    private ComboBox<String> emailUpdateParent;
     @FXML
     private PasswordField passwordUpdateParent;
 
@@ -27,9 +31,23 @@ public class UpdateParentView {
     private Button buttonSaveUpdateParent;
 
     @FXML
-    public void saveButtonClickedUpdateParent(ActionEvent event)
+    public void initialize(){
+        emailUpdateParent.setItems(FXCollections.observableArrayList(getParentEmails()));
+    }
+
+    private List<String> getParentEmails(){
+        List<String> parentEmails = new ArrayList<>();
+        for (TOParent parent: CoolSuppliesFeatureSet1Controller.getParents()){
+            parentEmails.add(parent.getEmail());
+        }
+        return parentEmails;
+    }
+
+    // ADD A CHECK FOR OLD PASSWORD TO SEE IF RIGHT PARENT 
+    @FXML
+    public void saveButtonClickedUpdateParent(javafx.event.ActionEvent event)
     {
-        String email = emailUpdateParent.getText();
+        String email = emailUpdateParent.getValue();
         String name = nameUpdateParent.getText();
         String phoneNumber = phoneUpdateParent.getText();
         int phone = Integer.parseInt(phoneNumber);
@@ -37,6 +55,11 @@ public class UpdateParentView {
 
         String message = CoolSuppliesFeatureSet1Controller.updateParent(email, password, name,phone);
         errorMessageUpdateParent.setText(message);
+        
+        emailUpdateParent.getSelectionModel().clearSelection();;
+        nameUpdateParent.setText("");
+        phoneUpdateParent.setText("");
+        passwordUpdateParent.setText("");
     }
 
 }
